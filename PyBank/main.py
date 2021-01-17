eb6fac34a4db424c80f81dd290289f569aaa4b61
Sharon Templin <sharon@hollygo.es>
2021-01-17 14:01:02 -0600

    Added the instructions for PyBank to main.py

---
+++ PyBank/main.py
@@ -0,0 +1,18 @@
+# This will analyze the financial records for ACME 
+
+# load the file
+
+# find the total number of months included in the dataset
+
+# find the net total amount of "Profit/Losses" over the entire period
+
+# calculate the changes in "Profit/Losses" over the entire period, 
+# then find the average of those changes
+
+# Find the greatest increase in profits (date and amount) over the entire period
+
+# Find the greatest decrease in losses (date and amount) over the entire period
+
+# Print the analysis to the terminal window
+
+# Export the analysis to a text file
