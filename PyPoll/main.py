b8a96ee7b2ae352ef1e49b38805068429e94f836
Sharon Templin <sharon@hollygo.es>
2021-01-19 14:18:39 -0600

    found the winner!

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,3 @@
-
-
 # Import the os and csv modules, bake some Chocolate Cake
 import os
 import csv
@@ -10,6 +8,7 @@ candidates =  []
 vote_tally = {}
 counties = []
 votes = 0
+county_tally = {}
 
 
 # load the file
@@ -17,13 +16,11 @@ with open(csvpath) as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    # print(csvreader) this is not needed for the final printout
 
     csv_header = next(csvreader)
 
-    print(f"CSV Header: {csv_header}")
-
-    
+    # print(f"CSV Header: {csv_header}") this is not needed for the final printout
 
      # iterate through the file... 
     for row in csvreader:
@@ -40,13 +37,15 @@ with open(csvpath) as csvfile:
             new_count = vote_tally[candidate] + 1
             vote_tally[candidate] = new_count
 
-        #if county not found in counties:
-         #   counties.append(county)
-        # find percentage of votes each candidate won
-        #vote_tally 
-
-        # find the winner of the election based on popular vote
-
+        if county not in counties:
+            counties.append(county)
+            county_tally[county] = 1
+        else:
+            new_tally = 0
+            new_tally = county_tally[county]
+            county_tally[county] = new_tally
+        
+        
 
 print(vote_tally)
 print(candidates)
@@ -56,7 +55,8 @@ print(sep_line)
 print(f"Total Votes: {total_number_votes}")
 print(sep_line)
 for candidate in candidates:
-    
+
+    # find percentage of votes each candidate won
     candidate_votes = vote_tally[candidate]
     candidate_percent = round(candidate_votes/total_number_votes*100,2)
 
@@ -64,7 +64,8 @@ for candidate in candidates:
 
 
 print(sep_line)
-
-#print(f"Winner: {}")
+# find the winner of the election based on popular vote
+winner = max(vote_tally, key=vote_tally.get)
+print(f"Winner: {winner}")
 print(sep_line)
 
